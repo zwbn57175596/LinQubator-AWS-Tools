22d3aa94c662b6039f59f77a3a11b9e7a534e055
zhaoweih <zhaoweih@chanjet.com>
2019-07-26 17:48:52 +0800

    add git

--- Ec2/install.sh
+++ Ec2/install.sh
@@ -2,6 +2,7 @@
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y install openjdk-8-jdk maven 
+sudo apt -y install git
 sudo apt -y install nodejs npm
 sudo npm install -g npm 
 sudo npm install -g n
