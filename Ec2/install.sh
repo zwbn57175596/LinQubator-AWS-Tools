e121cd6b88db7c0235c3d241967a9fa001dbda38
zhaoweih <zhaoweih@chanjet.com>
2019-07-27 16:08:34 +0800

    add nginx

--- Ec2/install.sh
+++ Ec2/install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 sudo apt update
 sudo apt -y upgrade
+
+
 sudo apt -y install openjdk-8-jdk maven 
 sudo apt -y install git
+
+
 sudo apt -y install nodejs npm
 sudo npm install -g npm 
 sudo npm install -g n
 sudo n stable
+
 sudo npm install nodemon -g
 sudo npm install -g babel-cli
 sudo npm install -g cross-env
+
+
 sudo apt -y install mysql-server
 sudo apt -y install redis
+sudo apt install nginx-full
 
