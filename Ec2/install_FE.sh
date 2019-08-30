964138b2e8278eb831eb7a0a02cf7e2bdecc2e96
zhaoweih <zhaoweih@chanjet.com>
2019-08-30 18:25:24 +0800

    add install_FE

---
+++ Ec2/install_FE.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update
+sudo apt -y upgrade
+
+
+sudo apt -y install git
+
+
+sudo apt -y install nodejs npm
+sudo npm install -g npm 
+sudo npm install -g n
+sudo n stable
+
+sudo npm install nodemon -g
+sudo npm install -g babel-cli
+sudo npm install -g cross-env
+
